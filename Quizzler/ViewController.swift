87b9f60684624be2bdb6a4209c294a953e63ef60
ckost9n <ckost9n@gmail.com>
2022-03-30 21:09:15 +0300

    Progress bar is active.  Add struct Question.

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -14,12 +14,54 @@ class ViewController: UIViewController {
     @IBOutlet var trueButton: UIButton!
     @IBOutlet var falseButton: UIButton!
     
+    private var quiz = [Question]()
+    private var delayTime = 0.3
+    
+    private var questionNumber = 0 {
+        didSet {
+            questionLabel.text = quiz[questionNumber].text
+            let progress = Float(questionNumber) / Float(quiz.count)
+            progressBar.setProgress(progress, animated: true)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupUI()
+        quiz = Question.getQuestion()
+        
+        questionLabel.text = quiz[questionNumber].text
+        
+    }
+    
+    private func setupUI() {
+        trueButton.layer.cornerRadius = 20
+        falseButton.layer.cornerRadius = 20
+        
+        progressBar.progress = 0
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        if sender.currentTitle == quiz[questionNumber].answer {
+            sender.backgroundColor = .green
+            DispatchQueue.main.asyncAfter(deadline: .now() + delayTime) {
+                sender.backgroundColor = .clear
+            }
+        } else {
+            sender.backgroundColor = .red
+            DispatchQueue.main.asyncAfter(deadline: .now() + delayTime) {
+                sender.backgroundColor = .clear
+            }
+        }
+        
+        if questionNumber < quiz.count - 1 {
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+        
         
     }
     
